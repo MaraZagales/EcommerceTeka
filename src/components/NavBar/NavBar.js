6b679d2f95add71754ebbc82dca5378085826c36
Mara Zagales <marazagales@gmail.com>
2022-07-25 17:54:37 -0300

    haciendo cambios en navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -11,25 +11,24 @@ const NavBar = () => {
   return (
     <div>
       <Navbar expand="lg" className="navBar">
-        
         <Container>
-          <Navbar.Brand href="#home"><img
+          <Navbar.Brand href="#home" className="logoNav"><img
           src="/assets/logoTeka.jpg"
           alt="logo de Teka"
-          className="logoNav"
+          
         /> </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto containerNav">
-              <Nav.Link href="#home">Inicio</Nav.Link>
-              <NavDropdown title="Productos" id="basic-nav-dropdown">
+            <Nav className="me-auto">
+              <Nav.Link className="navLink" href="#home">Inicio</Nav.Link>
+              <NavDropdown className="navLink" title="Productos" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Sillas</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Mesas</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Rack TV</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.4">Sillones</NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link href="#link">¿Cómo Comprar?</Nav.Link>
-              <Nav.Link href="#link">Contactanos</Nav.Link>
+              <Nav.Link className="navLink"href="#link">¿Cómo Comprar?</Nav.Link>
+              <Nav.Link className="navLink"href="#link">Contactanos</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
